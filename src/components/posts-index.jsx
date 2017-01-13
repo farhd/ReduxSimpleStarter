cbbcf9ab9592c8e50515db057c61b01fbf66421d
Farhad Abdullayev <farhad@dimedis.de>
2017-01-13 10:41:31 +0100

    adjust according eslint-arbnb

---
+++ src/components/posts-index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+
+class PostIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+// function mapDispatchToProps (dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch)
+// }
+
+export default connect(null, { fetchPosts })(PostIndex);
---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/posts-index';
+
+const Greeting = () => <div>Hey there!</div>;
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={PostsIndex} />
+    <Route path="greet" component={Greeting} />
+  </Route>
+);
